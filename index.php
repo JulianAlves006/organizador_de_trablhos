e5862a4cb9ff9a0a1f5eed5b1947161b92ff417b
Julian Alves <julianalvesinstinto@gmail.com>
2023-09-26 17:30:46 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Organizador de trabalhos</title>
+    <link rel="stylesheet" href="frontEnd/css/style.css">
+    <script src="frontEnd/js/main.js"></script>
+</head>
+<body>
+    <header>
+        <h1>Título</h1>
+        <nav>
+        <ul>
+        <li>HOME</li>
+        <li>SOBRE</li>
+        <li>BLOG</li>
+        </ul>
+        </nav>
+    </header>
+    <section>
+        <h1>Ola seja bem vindo ao seu melhor organizador de trabalhos!!</h1>
+        <h2>Aqui você podera organizar seus trabalhos da escola, faculdade, emprego, etc...</h2>
+    </section>
+</body>
+</html>
